5dea7896827559f0cc84e1235c844d0dc8a7ff74
omar <omarliberatto1967@gmail.com>
2024-06-07 10:58:37 -0300

    nuevos graficos

--- dist/js/pages/dashboard3.js
+++ dist/js/pages/dashboard3.js
@@ -32,30 +32,21 @@ $(function () {
 		data: {
 			labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'ENE'],
 
-			datasets: [
-				{
-					backgroundColor: '#92C01E', borderColor: '#92C01E',
-					data: [100, 200, 300, 250, 270, 250, 300, 340]
-				}, // sh
+			datasets: [{
+				backgroundColor: '#78B258', borderColor: '#78B258',
+				data: [100, 200, 300, 250, 270, 250, 300, 340]
+			}, // sh
 
-				{
-					backgroundColor: '#ced4da', borderColor: '#ced4da',
-					data: [700, 170, 270, 200, 180, 150, 200, 275]
-				}, // woo
+			{
+				backgroundColor: '#0050C3', borderColor: '#0050C3',
+				data: [700, 170, 270, 200, 180, 150, 200, 275]
+			}, // tn
 
-				{
-					backgroundColor: '#ced4df', borderColor: '#ced4df',
-					data: [700, 170, 270, 200, 180, 150, 200, 380]
-				},  // TN
+			{
+				backgroundColor: '#9A5C8E', borderColor: '#9A5C8E',
+				data: [700, 170, 270, 200, 180, 150, 200, 380]
+			}  // wc
 
-				{
-					backgroundColor: '#007bff', borderColor: '#007bff',
-					data: [100, 200, 300, 250, 270, 250, 300, 550]
-				} // ID
-				{
-					backgroundColor: '#92C01E', borderColor: '#92C01E',
-					data: [100, 200, 300, 250, 270, 250, 300, 340]
-				}, // sh
 
 			]
 		},
@@ -102,18 +93,19 @@ $(function () {
 		data: {
 			labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
 
-			datasets: [{
-				type: 'line', data: [100, 120, 170, 167, 180, 177, 160],
-
-				backgroundColor: 'transparent', borderColor: '#007bff', pointBorderColor: '#007bff', pointBackgroundColor: '#007bff', fill: false
-			},
-
-
-
-			{
-				type: 'line', data: [60, 80, 70, 67, 80, 77, 100],
-				backgroundColor: 'tansparent', borderColor: '#ced4da', pointBorderColor: '#ced4da', pointBackgroundColor: '#ced4da', fill: false
-			}]
+			datasets: [
+				{
+					type: 'line', data: [100, 120, 170, 167, 180, 177, 160],
+					backgroundColor: 'transparent', borderColor: '#78B258', pointBorderColor: '#78B258', pointBackgroundColor: '#78B258', fill: false
+				},
+				{
+					type: 'line', data: [40, 150, 140, 175, 225, 210, 195],
+					backgroundColor: 'transparent', borderColor: '#0050C3', pointBorderColor: '#0050C3', pointBackgroundColor: '#0050C3', fill: false
+				},
+				{
+					type: 'line', data: [60, 80, 70, 67, 80, 77, 100],
+					backgroundColor: 'tansparent', borderColor: '#9A5C8E', pointBorderColor: '#9A5C8E', pointBackgroundColor: '#9A5C8E', fill: false
+				}]
 		},
 		options: { maintainAspectRatio: false, tooltips: { mode: mode, intersect: intersect }, hover: { mode: mode, intersect: intersect }, legend: { display: false }, scales: { yAxes: [{ gridLines: { display: true, lineWidth: '4px', color: 'rgba(0, 0, 0, .2)', zeroLineColor: 'transparent' }, ticks: $.extend({ beginAtZero: true, suggestedMax: 200 }, ticksStyle) }], xAxes: [{ display: true, gridLines: { display: false }, ticks: ticksStyle }] } }
 	})
