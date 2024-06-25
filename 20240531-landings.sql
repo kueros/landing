88aa4e17e56aeee8bb8ea217fdf4c328ed798f17
iFLOW eCOMMERCE <86629788+pdiblasiiflow21@users.noreply.github.com>
2024-06-25 10:43:48 -0300

    modified:   20240531-landings.sql 	deleted:    api/services/v1/functions.php 	deleted:    api/services/v1/track_order.php 	modified:   assets/img/logoIf.png 	modified:   config/conexion.php 	modified:   css/app.css

--- 20240531-landings.sql
+++ 20240531-landings.sql
@@ -20,3 +20,28 @@ CREATE TABLE `landings` (
 LOCK TABLES `landings` WRITE;
 INSERT INTO `landings` VALUES (1,'shopify','sf_dash','sf_inst','2024-05-31 13:08:00','2024-05-31 13:08:00'),(2,'tiendanube','tn_dash','tn_inst','2024-05-31 13:08:00','2024-05-31 13:08:00'),(3,'magento','magento_dash','magento_inst','2024-05-31 13:08:00','2024-05-31 13:08:00'),(4,'woocommerce','wc_dash','wc_inst','2024-05-31 13:08:00','2024-05-31 13:08:00'),(5,'integracion directa','id_dash','id_inst','2024-05-31 13:08:00','2024-05-31 13:08:00');
 UNLOCK TABLES;
+
+
+
+---
+--- Creacion de Vistas para el tablero.
+---
+
+/*DROP TABLE IF EXISTS `v_ord_create_all`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `v_ord_create_all` AS select ((select `woo_order`.`id_tienda` from `woo_order`) + (select `order_in`.`store_id` from `order_in`)) AS `tiendas`,((select `woo_order`.`tracking_id` from `woo_order`) + (select `order_in`.`order_id` from `order_in`)) AS `etiqueta`,((select `woo_order`.`shipment_id` from `woo_order`) + (select `order_in`.`order_nro` from `order_in`)) AS `nro_orden`,((select `woo_order`.`code` from `woo_order`) + (select `order_in`.`respcode` from `order_in`)) AS `codigo`,((select `woo_order`.`updated_at` from `woo_order`) + (select `order_in`.`updated_at` from `order_in`)) AS `fecha`;
+*/
+
+CREATE VIEW shf_last_3_month AS
+SELECT * FROM order_in WHERE updated_at >= DATE_SUB(NOW(), INTERVAL 3 MONTH);
+
+
+CREATE VIEW woo_last_3_month AS
+SELECT * FROM woo_order WHERE updated_at >= DATE_SUB(NOW(), INTERVAL 3 MONTH);
+
+
+
+
+
+CREATE VIEW v_count_orders
+select COUNT(*) from `woo_order` WHERE tracking_id IS  NULL) + (select COUNT(*) from `order_in` WHERE trackid IS  NULL )) AS `CREADAS`,
+select COUNT(*) from `woo_order` WHERE tracking_id IS  NULL) + (select COUNT(*) from `order_in` WHERE trackid IS  NULL )) AS `NO CREADAS`;
--- config/conexion.php
+++ config/conexion.php
@@ -14,3 +14,22 @@ if (mysqli_connect_errno()) {
     exit();
 }
 mysqli_set_charset($con, 'utf8');
+
+// conexion PDO a base
+
+$host = DB_SERVER;
+$dbname =DB_NAME;
+$username = DB_USER;
+$password = DB_PASS;
+try {
+    // Crear una instancia de PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // Configurar PDO para que lance excepciones en caso de error
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "Conexión exitosa";
+} catch (PDOException $e) {
+    // Manejar errores de conexión
+    echo "Error de conexión: " . $e->getMessage();
+}
